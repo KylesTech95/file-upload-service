2e3f41a577af30d145fb0a646447bd17a002e028
kylestech95 <kylestech95@outlook.com>
2024-02-10 17:18:12 -0500

    feat: post uploaded file

--- index.js
+++ index.js
@@ -4,9 +4,14 @@ require('dotenv').config()
 require('dotenv').config()
 const multer = require('multer')
 const up = multer({dest:'uploads/'})
+const bP = require('body-parser')
 var app = express();
 
 app.use(cors());
+app.use(express.urlencoded({extended:true}))
+app.use(bP.urlencoded({extended:true}))
+app.use(express.json())
+app.use(bP.json())
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
@@ -15,7 +20,10 @@ app.get('/', function (req, res) {
 
 
 app.post("/api/fileanalyse", up.single('upfile'),(req,res)=>{
-  console.log(res.file)
+  console.log(req.file)
+
+
+  return !req.file ? res.send("Nothing was uploaded") : res.json({ name:req.file.filename, type:req.file.mimetype, size:req.file.size })
 })
 
 
